95b80114b52da902aebcc00302f107c1b6e0a004
Vipul Mishra <vipulmishra8591@gmail.com>
2026-03-07 01:37:43 +0530

    Add home page route

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchFootwear = async () => {
       try {
-        const res = await api.get("/footwear");
+        const res = await api.get("/");
         setFootwear(res.data);
       } catch (error) {
         toast.error("Failed to load footwear");
